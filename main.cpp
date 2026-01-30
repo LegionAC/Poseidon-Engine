bd5f2f7f590d6bd02651a757f3b40cf5254871d6
LegionAC <battletowere9@gmail.com>
2026-01-30 22:07:14 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,12 +2,19 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <vector>
+#include <math.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "uniform mat4 model;\n"
+    "uniform mat4 view;\n"
+    "uniform mat4 projection;\n"
     "void main()\n"
     "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
     "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
@@ -15,7 +22,7 @@ const char *fragmentShaderSource = "#version 330 core\n"
 
     "void main()\n"
     "{\n"
-    "FragColor = vec4(0.1, 0.1, 0.1, 0.1);\n"
+    "FragColor = vec4(1.0, 1.0, 1.0, 1.0);\n"
     "}\0";
 
 unsigned int shaderProgram;
@@ -24,19 +31,21 @@ unsigned int VBO;
 
 std::vector< std::vector<float> > verticesContainer;
 
-std::vector<float> vertices {
-    -0.9f, -0.5f, 0.0f,
-    0.5f, -0.5f, 0.0f,
-    0.0f,  0.5f, 0.0f
-};
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
 
-std::vector<float> vertices2 {
-    -0.8f, -0.1f, 0.3f,
-    0.21f, -0.12f, 0.5f,
-    0.0f, 0.0f, 0.0f
-};
+glm::mat4 view;
 
-int initialize(std::vector<float>* verticesVector, unsigned int verticeBytes) {
+float lastFrame = 0.0f;
+float deltaTime = 0.0f;
+
+float yaw = -90.0f;
+float pitch = 0.0f;
+
+bool firstMouse = true;
+
+int initialize(std::vector<float>* verticesVector, unsigned int verticesBytes) {
 
     float* vertices = verticesVector->data();
 
@@ -72,7 +81,7 @@ int initialize(std::vector<float>* verticesVector, unsigned int verticeBytes) {
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, verticeBytes, vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, verticesBytes, vertices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -85,43 +94,175 @@ int initialize(std::vector<float>* verticesVector, unsigned int verticeBytes) {
     return 0;
 }
 
-int renderObject(unsigned int shaderProgram, unsigned int VAO) {
+int renderObject(unsigned int shaderProgram, unsigned int VAO, size_t vectorSize) {
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLES, 0, vectorSize / 3);
 
+    return 0;
+}
+
+int renderCircle(unsigned int resolution, std::vector<float> originVertices, float radius, unsigned int renderedWidth, unsigned int renderedHeight) {
+
+    std::vector<float> vertices;
+
+    float x = originVertices[0];
+    float y = originVertices[1];
+    float z = originVertices[2];
+
+    float operation = 2.0f * 3.1415926f / resolution;
+
+    for (int i{0}; i < resolution; i++) {
+        float aspectRatio = (float)renderedWidth / (float)renderedHeight;
+        float angle1 = i * operation;
+        float angle2 = (i + 1) * operation;
+
+        vertices.push_back(x);
+        vertices.push_back(y);
+        vertices.push_back(z);
+
+        vertices.push_back(x + cos(angle1) * radius);
+        vertices.push_back(y + sin(angle1) * radius);
+        vertices.push_back(z);
+
+        vertices.push_back(x + cos(angle2) * radius);
+        vertices.push_back(y + sin(angle2) * radius);
+        vertices.push_back(z);
+    }
+
+    verticesContainer.push_back(vertices);
     return 0;
 }
 
 struct objData {
     unsigned int VAO;
     unsigned int shaderProgram;
+    size_t vectorSize;
 };
 
-int renderViewport(GLFWwindow* userInterface) {
+float lastX = 800.0f, lastY = 600.0f;
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+  
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; 
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw   += xoffset;
+    pitch += yoffset;
+
+    if(pitch > 89.0f)
+        pitch = 89.0f;
+    if(pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(direction);
+
+}
+
+void movementHandler(GLFWwindow* window) {
+    view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+    float currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
+    float cameraSpeed = 2.5f * deltaTime;
+
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        cameraSpeed = cameraSpeed / 2.0f;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraUp;
+    if (glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraUp;
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+}
+
+int renderViewport(GLFWwindow* userInterface, unsigned int renderedWidth, unsigned int renderedHeight) {
+    std::vector<float> vertices {
+     1.0f,  1.0f,  1.0f,
+     1.0f, -1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+    
+     1.0f,  1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f, -1.0f,
+    
+     1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+    
+     1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+    -1.0f,  1.0f, -1.0f
+    };
+
     verticesContainer.push_back(vertices);
-    verticesContainer.push_back(vertices2);
+
+    renderCircle(1000, std::vector<float> {0.0f, 0.0f, 0.0f}, 0.1, renderedWidth, renderedHeight);
 
     std::vector<objData> objsData;
 
     for (std::vector<float> v : verticesContainer) {
-        initialize(&v, sizeof(v));
+        initialize(&v, v.size() * sizeof(float));
         objData obj;
         obj.VAO = VAO;
         obj.shaderProgram = shaderProgram;
+        obj.vectorSize = v.size();
         objsData.push_back(obj);
     }
 
     while (!glfwWindowShouldClose(userInterface)) {
+        movementHandler(userInterface);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 
+        (float)renderedWidth / (float)renderedHeight, 0.1f, 100.0f);
+
         for (objData v : objsData) {
-            renderObject(v.shaderProgram, v.VAO);
+            glUseProgram(v.shaderProgram);
+        
+            unsigned int modelLoc = glGetUniformLocation(v.shaderProgram, "model");
+            unsigned int viewLoc = glGetUniformLocation(v.shaderProgram, "view");
+            unsigned int projLoc = glGetUniformLocation(v.shaderProgram, "projection");
+        
+            glm::mat4 model = glm::mat4(1.0f);
+            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+            glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+        
+            renderObject(v.shaderProgram, v.VAO, v.vectorSize);
         }
+
+
         glfwSwapBuffers(userInterface);
         glfwPollEvents();
-
-        if (glfwGetKey(userInterface, GLFW_KEY_F6) == GLFW_PRESS) {
-        }
 
     }
     return 0;
@@ -129,6 +270,7 @@ int renderViewport(GLFWwindow* userInterface) {
 
 int interface() {
     glfwInit();
+
     GLFWmonitor* primary = glfwGetPrimaryMonitor();
     const GLFWvidmode* mode = glfwGetVideoMode(primary);
 
@@ -141,7 +283,12 @@ int interface() {
 
     gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    renderViewport(userInterface);
+    unsigned int renderedWidth = 2560;
+    unsigned int renderedHeight = 1440;
+
+    glViewport(0, 0, renderedWidth, renderedHeight); // PORTION OF SCREEN THAT RENDERING WORKS ACROSS, CURRENTLY USER RESOLUTION
+
+    renderViewport(userInterface, renderedWidth, renderedHeight);
 
     return 0;
 }
